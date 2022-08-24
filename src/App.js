54e06aec998cb222e0def18954db9e9df1e080e6
alexoxa1 <pugachov7753191@gmail.com>
2022-08-24 14:17:15 +0300

    adding useEffect functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TodoList from './TodoList'
 
 export default function App() {
-  const [todos, setTodos] = useState([
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
-  ])
-
+  const [todos, setTodos] = useState([])
   const [todoTitle, setTodoTitle] = useState("");
+
+  const handleClilck = () => console.log("clilck");
+
+  useEffect(() => {
+    try {
+      const raw = localStorage.getItem("todos") || []
+      setTodos(JSON.parse(raw))
+    } catch (err) {
+      // 👇️ SyntaxError: Unexpected end of JSON input
+      console.log('error', err);
+    }
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener("click", handleClilck)
+    localStorage.setItem("todos", JSON.stringify(todos));
+    return () => {
+      document.removeEventListener("click", handleClilck);
+    }
+  }, [todos])
 
   const addTodo = event => {
     if (event.key === "Enter") {
